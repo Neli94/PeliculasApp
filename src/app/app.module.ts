38156b187a324e73ca22d3d13242977741651cb4
Nelionic <negomasoyyo@gmail.com>
2019-04-22 15:28:12 +0200

    pelisapp:peliculas service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { PeliculaComponent } from './components/pelicula/pelicula.component';
+
+// Services
+import { PeliculasService } from './services/peliculas.service';
+
+// Rutas
+import { APP_ROUTING } from './app.routes';
+import { RouterModule } from '@angular/router';
+import { BuscarComponent } from './components/buscar/buscar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    PeliculaComponent,
+    BuscarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, APP_ROUTING
   ],
-  providers: [],
+  providers: [PeliculasService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/peliculas.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { Jsonp } from '@angular/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PeliculasService {
+  private apiKey = '5294b41adf348df0ac2dbdcf65e50d8e';
+  private urlMovieDb = 'https://api.themoviedb.org/3';
+
+  constructor( private jsonp: Jsonp ) { }
+
+  getCartelera() {
+    const desde = new Date();
+    const hasta = new Date();
+    hasta.setDate(hasta.getDate() + 7);
+
+    const desdeStr = `${desde.getFullYear()}-${desde.getMonth() + 1}-${desde.getDay()}`;
+    const hastaStr = `${desde.getFullYear()}-${desde.getMonth() + 1}-${desde.getDay()}`;
+
+    // tslint:disable-next-line:max-line-length
+    const url = `${this.urlMovieDb}/discover/movie?primary_release_date.gte=${desdeStr}&primary_release_date.lte=${hastaStr}&api_key=${this.apiKey}&language=es&callback=JSONP_CALLBACK`;
+    return this.jsonp.get(url).pipe(
+      map( res => {
+        return res;
+    }));
+  }
+
+  getPopulares() {
+    const url = `${this.urlMovieDb}/discover/movie?sort_by=popularity.desc&api_key=${this.apiKey}&language=es&callback=JSONP_CALLBACK`;
+    return this.jsonp.get(url).pipe(
+      map( res => {
+        return res;
+    }));
+  }
+
+  buscarPelicula( texto: string ) {
+    // tslint:disable-next-line:max-line-length
+    const url = `${this.urlMovieDb}/search/movie?query=${texto}&sort_by=popularity.desc&api_key=${this.apiKey}&language=es&callback=JSONP_CALLBACK`;
+    return this.jsonp.get(url).pipe(
+      map( res => {
+        return res;
+    }));
+  }
+
+}
